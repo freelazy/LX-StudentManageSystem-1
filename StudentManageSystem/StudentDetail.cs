7b1f51e9ff2c1e862264eaa341bf24b4f9fa6ec0
yao7san <yao7san@fake.com>
2022-09-14 15:24:38 +0800

    fix

--- StudentManageSystem/StudentDetail.cs
+++ StudentManageSystem/StudentDetail.cs
@@ -26,25 +26,6 @@ namespace StudentManageSystem
             var student = dt.Rows[0];
 
             // 第二步，拼接出合适的 html 页面
-            var studentStr = $@"
-<ul>
-   <li>学号: {student[0]}</li>
-   <li>姓名: {student[1]}</li>
-   <li>地址: {student[2]}</li>
-   <li>电话: {student[7]}</li>
-</ul>
-<div>
-   <button onclick='goHome()'>点我返回首页</button>
-</div>
-<script>
-   function goHome () {{
-      //if (window.confirm('是不是确定要返回首页?')) {{
-         window.location.href = '/students';
-      //}}
-   }}
-</script>
-";
-            
             var html = $@"
 <html>
 <head>
@@ -52,8 +33,23 @@ namespace StudentManageSystem
 </style>
 </head>
 <body>
-  <h3> 学生详细信息 </h3>
-{studentStr}
+    <h3> 学生详细信息 </h3>
+    <ul>
+       <li>学号: {student[0]}</li>
+       <li>姓名: {student[1]}</li>
+       <li>地址: {student[2]}</li>
+       <li>电话: {student[7]}</li>
+    </ul>
+    <div>
+       <button onclick='goHome()'>点我返回首页</button>
+    </div>
+    <script>
+       function goHome () {{
+          //if (window.confirm('是不是确定要返回首页?')) {{
+             window.location.href = '/students';
+          //}}
+       }}
+    </script>
 </body>
 </html>";
 
