7dc00da4ee748c0eb0096dcfba40b291ab40c4d7
yao7san <yao7san@fake.com>
2022-09-14 11:29:57 +0800

    调整

--- StudentManageSystem/StudentList.cs
+++ StudentManageSystem/StudentList.cs
@@ -14,7 +14,6 @@ namespace StudentManageSystem
 
         public void ProcessRequest(HttpContext context)
         {
-            // TODO:
             // 第一步，将所有学生，从数据库中读取出来
             var connStr = ConfigurationManager.ConnectionStrings["mydb"].ConnectionString;
             var sql = "select * from students";
@@ -23,17 +22,20 @@ namespace StudentManageSystem
             var dt = new DataTable();
             adapter.Fill(dt);
 
-            // 第二步，拼接出合适的 html 页面，返回给请求者
-            var head = "<tr><th>姓名</th><th>地址</th></tr>";
+            // 第二步，拼接出合适的 html 页面
             var rows = String.Empty;
             foreach (DataRow dr in dt.Rows)
             {
                 rows += $"<tr><td>{dr[1]}</td><td>{dr[2]}</td></tr>\n";
             }
+            var head = "<tr><th>姓名</th><th>地址</th></tr>";
             var table = $"<table>{head}\n{rows}</table>";
 
             var css = "<style>table, th, td { border: 1px solid black; border-collapse: collapse; padding: 5px 2em }</style>";
-            context.Response.Write($"<html><head>{css}</head><body>{table}</body></html>");
+            var html = $"<html><head>{css}</head><body>{table}</body></html>";
+
+            // 第三步，返回给请求者
+            context.Response.Write(html);
         }
      }
 }
