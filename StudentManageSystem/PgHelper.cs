d66e00ff8c20caec58ad2643c602621cb24eec6b
yao7san <yao7san@fake.com>
2022-09-16 15:23:10 +0800

    继续偷懒: 通过 html 文件的格式，简化字符串拼接

---
+++ StudentManageSystem/PgHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace StudentManageSystem
+{
+    public class PgHelper
+    {
+        public static string GetTemplate(string path, HttpRequest request)
+        {
+            // 1.获取 student-update.html 文件的绝对路径
+            string file = $"{request.PhysicalApplicationPath}html\\{path}";
+            // 2. 通过 API 将文件中所有内容读取为字符串
+            string context = File.ReadAllText(file);
+            // 3. 将字符串返回
+            return context;
+        }
+
+        public static string GetHtml(string path, HttpRequest request, params object[] ds)
+        {
+            var tpl = GetTemplate(path, request);
+            return string.Format(tpl, ds)
+                .Replace('[', '{')
+                .Replace(']', '}');
+        }
+    }
+}
--- StudentManageSystem/StudentDetail.cs
+++ StudentManageSystem/StudentDetail.cs
@@ -22,32 +22,12 @@ namespace StudentManageSystem
                 .Rows[0];
 
             // 第二步，拼接出合适的 html 页面
-            var html = $@"
-<html>
-<head>
-<style>
-</style>
-</head>
-<body>
-    <h3> 学生详细信息 </h3>
-    <ul>
-       <li>学号: {student[0]}</li>
-       <li>姓名: {student[1]}</li>
-       <li>地址: {student[2]}</li>
-       <li>电话: {student[7]}</li>
-    </ul>
-    <div>
-       <button onclick='goHome()'>点我返回首页</button>
-    </div>
-    <script>
-       function goHome () {{
-          //if (window.confirm('是不是确定要返回首页?')) {{
-             window.location.href = '/students';
-          //}}
-       }}
-    </script>
-</body>
-</html>";
+            var html = PgHelper.GetHtml("student-detail.html",
+                context.Request,
+                student[0],
+                student[1],
+                student[2],
+                student[7]);
 
             // 第三步，返回给请求者
             context.Response.Write(html);
