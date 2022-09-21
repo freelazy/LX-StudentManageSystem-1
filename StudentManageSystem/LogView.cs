450ce65b9fe5288c412dda2c5966c569abd755b7
yao7san <yao7san@fake.com>
2022-09-21 11:45:18 +0800

    简单实现日志查看的功能

---
+++ StudentManageSystem/LogView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace StudentManageSystem
+{
+    public class LogView : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var rows = DbHelper
+                .DoExecuteQuery("select * from request_log order by created desc")
+                .Rows
+                .Cast<DataRow>()
+                .Select(dr => $@"<div class='log-item'><span>{dr[1]}</span><span>{dr[3]}</span><span>{dr[4]}</span><span>{dr[2]}</span></div>")
+                .ToArray();
+
+            var html = PgHelper.GetHtml("log-view.html", string.Join("\n", rows));
+
+            context.Response.Write(html);
+        }
+     }
+}
